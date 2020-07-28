da2d0c3865ab6672fa30fdc358ea08865c37fc75
Jujo4 <61561480+Jujo4@users.noreply.github.com>
2020-07-28 11:12:25 +0200

    Add files via upload

--- Funciones/ft_strjoin.c
+++ Funciones/ft_strjoin.c
@@ -7,25 +7,17 @@ char *ft_strjoin(char const *s1, char const *s2)
 {
 	char *s;
 	size_t i;
-	size_t j;
 	size_t len;
 
 	i = 0;
-	j = 0;
-	len = ft_strlen(s1) + ft_strlen(s2);
-	if(!(s = (char *) malloc(sizeof(char) * len + 1)));
-		return (0);
-	while (s1)
-	{
-		s[i] = s1[i];
-		i++;
-	}
-	while (s2)
-	{
-		s[i] = s2[j];
-		i++;
-		j++;
-	}
+	len = strlen(s1) + strlen(s2);
+	
+	if(!(s = (char *)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	while (*s1 != '\0')
+		s[i++] = *s1++;
+	while (*s2 != '\0')
+		s[i++] = *s2++;
 	s[i] = '\0';
 	return (s);
 }
