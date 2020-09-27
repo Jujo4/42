e495b0556cb8693405f4f7eb725e8af05b696bd1
Juan jose Sanchez diez <jsanchez@c2r5s6.42madrid.com>
2020-09-27 21:42:23 +0200

    Day 2

--- Funciones/ft_strjoin.c
+++ Funciones/ft_strjoin.c
@@ -6,27 +6,32 @@
 /*   By: jsanchez <jsanchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/28 13:00:45 by jsanchez          #+#    #+#             */
-/*   Updated: 2020/09/23 12:31:24 by jsanchez         ###   ########.fr       */
+/*   Updated: 2020/09/27 21:08:49 by jsanchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
-{
+{	
 	char	*s;
 	size_t	i;
-	size_t	len;
 
 	i = 0;
-	len = ft_strlen(s1) + ft_strlen(s2);
-
-	if (!(s = (char *)malloc(sizeof(char) * len + 1)))
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	if (!(s = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1)))
 		return (NULL);
 	while (*s1 != '\0')
-		s[i++] = *s1++;
+	{
+		s[i] = *s1++;
+		++i;
+	}
 	while (*s2 != '\0')
-		s[i++] = *s2++;
+	{
+		s[i] = *s2++;
+		++i;
+	}
 	s[i] = '\0';
 	return (s);
 }
--- Funciones/ft_substr.c
+++ Funciones/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jsanchez <jsanchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/23 12:14:42 by jsanchez          #+#    #+#             */
-/*   Updated: 2020/09/23 12:14:42 by jsanchez         ###   ########.fr       */
+/*   Updated: 2020/09/27 21:40:17 by jsanchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*s2;
 
-	if (!(s2 = (char *)malloc(len)))
-		return (NULL);
 	i = 0;
+	if (s == NULL)
+		return (NULL);
+	if (!(s2 = (char *)malloc(sizeof(char) * ft_strlen(s) - 1)))
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (NULL);
 	while (i < len)
 	{
 		s2[i] = s[start + i];
