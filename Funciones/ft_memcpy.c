466ab76a19eee9ba5e348c3e237f2a1fce59f417
Jujo4 <61561480+Jujo4@users.noreply.github.com>
2020-10-22 00:32:04 +0200

    Add files via upload

--- Funciones/ft_memcpy.c
+++ Funciones/ft_memcpy.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsanchez <jsanchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/30 10:41:07 by jsanchez          #+#    #+#             */
-/*   Updated: 2020/09/23 12:12:36 by jsanchez         ###   ########.fr       */
+/*   Created: 2020/10/22 00:15:05 by jsanchez          #+#    #+#             */
+/*   Updated: 2020/10/22 00:15:05 by jsanchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t i;
 
